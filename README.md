fd2e994770788d18f930206ff1f812a39873819c
jfx <jfxmate@gmail.com>
2020-09-23 19:18:04 +1000

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# pacman-contest-replay-player
+
+A script to make it easier to play Pacman contest replays.
+
+Please make sure you have the latest capture changes from Sebastian: https://piazza.com/class/kbsmlzxg3k7418?cid=225 (@225).
+
+## Configuration
+
+The python command can be changed in the script, as well as the path to the replays folder.
+
+By default the script looks for replays in the ``replays`` folder and runs the ``capture.py`` script by calling ``python``.
+
+## Usage
+
+For example usage
+```
+python replay-player.py --help
+```
+
+Running the script without any parameters will just return all files in the replays folder specified with their corresponding id
+```
+python replay-player.py
+```
+
+You can filter by a team or by two teams by using the `-t` or `-teams` flag. Note this flag is completely optional.
+```
+python replay-player.py --team TEAM1
+
+python replay-player.py --team TEAM1 TEAM2
+```
+
+Afterwards you can select a replay ID by using the `-n` or `--number` flag
+```
+python replay-player.py --team TEAM1 TEAM2 -n 1
+
+python replay-player.py -n 1
+```
+
+## Notes
+
+You may want to add the ``replays`` folder to your `.gitignore`. You can do this by adding ``replays/`` to your `.gitignore` file.
---
+++ replay-player.py
@@ -0,0 +1,54 @@
+from os import listdir
+from os.path import isfile, join, exists
+import sys
+import argparse
+import os
+import re
+
+# Settings
+REPLAYS_FOLDER = "replays"
+PYTHON = "python36"
+
+# Constants
+DIR_SCRIPT = sys.path[0]
+REPLAYS_FOLDER = join(DIR_SCRIPT, REPLAYS_FOLDER)
+
+# Arg parsing
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--teams', dest='teams', nargs='+', help='Set teams to filter by <team1> [team2]')
+parser.add_argument('-n','--number', dest='number', nargs='?', type=int, help='Set replay to run. Omit to list all replays')
+parser.add_argument('-s', '--delay-step', type=float, dest='delay_step', help='Delay step in a play or replay.', default=0.03)
+args = parser.parse_args()
+
+def main():
+	if not exists(REPLAYS_FOLDER):
+		return print('No replays folder found with path "{}". This can be edited in the script under REPLAYS_FOLDER'.format(REPLAYS_FOLDER))
+
+	all_files = [f for f in listdir(REPLAYS_FOLDER) if isfile(join(DIR_SCRIPT, REPLAYS_FOLDER, f))]
+	
+	# Team name filtering code
+	if args.teams and len(args.teams) == 1: # 1 team given
+		files = [k for k in all_files if args.teams[0] in k]
+	elif args.teams and len(args.teams) > 1: # 2 teams given
+		files = [k for k in all_files if args.teams[0] in k and args.teams[1] in k]
+	else: # No team filtering
+		files = all_files
+
+	if len(files) == 0:
+		print("ERROR: No files found")
+	elif args.number: # We are selecting a replay to play
+		if args.number < len(files) + 1:
+			found_file = files[args.number - 1]
+			match = re.match("(.*)_vs_(.*)_.*", found_file)
+			red = match[1]
+			blue = match[2]
+			os.system("{} {} --red-name {} --blue-name {} --replay {}/{} --delay-step {}".format(PYTHON, join(DIR_SCRIPT, "capture.py"), red, blue, REPLAYS_FOLDER, found_file, str(args.delay_step)))
+		else:
+			print("ERROR: Invalid replay ID. {} replays found (select from 1 - {}).".format(len(files), len(files)))
+	else: # No number given, list the IDs
+		print("{} Files found:".format(len(files)))
+		for i, f in enumerate(files):
+			print("ID {}: {}".format(i + 1, f))
+
+if __name__ == "__main__":
+    main()
